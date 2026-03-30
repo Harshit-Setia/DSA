554f2818d800195d43f815bc845d31071e66650d
Harshit <harshitsetia.005@gmail.com>
2026-03-30 15:13:06 +0530

    init

--- class/KthSmallestInMatrix.java
+++
@@ -1,8 +0,0 @@
-public class KthSmallestInMatrix {
-
-    public static void main(String[] args) {
-        int [][]arr={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-        int k=8;
-        System.out.println(arr[k/arr.length][(k%arr.length)]);
-    }
-}
